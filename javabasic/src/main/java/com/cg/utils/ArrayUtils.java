9121315ca0a508819ecd631a41063926948ea3e6
quangdangcodegym <quang.dang@codegym.vn>
2024-01-17 14:32:02 +0700

    array insert

--- javabasic/src/main/java/com/cg/utils/ArrayUtils.java
+++ javabasic/src/main/java/com/cg/utils/ArrayUtils.java
@@ -2,11 +2,13 @@ package com.cg.utils;
 
 public class ArrayUtils {
     public static void main(String[] args) {
-        int[] arr = {5, -1, 8, 3, 6};               // 0x32cc: {5, -1, 8, 3, 6}
+        int[] arr = {5, -7, 8, 3};               // 0x32cc: {3, -1, 5, 6, 8, -7}
 
+        int[] result = insertLast(arr, 10);
+        printArray(result);
         // arr: 0x32cc
-        interchangeSort(arr);
-        printArray(arr);
+//        interchangePositiveSort(arr);
+//        printArray(arr);
 
     }
     public static void printArray(int [] numbers){
@@ -25,5 +27,29 @@ public class ArrayUtils {
                 }
             }
         }
+    }
+    public static void interchangePositiveSort(int [] numbers){
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > 0 && numbers[j] > 0 && numbers[j] < numbers[i]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static int [] insertLast(int [] numbers, int value){
+        int [] arrNew = new int[numbers.length + 1];            // arrNew: [0, 0, 0, 0, 0]
+        for (int i = 0; i < numbers.length; i++) {
+            arrNew[i] = numbers[i];
+        }
+        arrNew[arrNew.length - 1] = value;
+        return arrNew;
+    }
+
+    public static int[] insert(int[] numbers, int value, int index) {
+        return null;
     }
 }
